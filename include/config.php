d0bc61ca80c873154af6303c5d2573a43366bd86
tahir-official <tahirk.official@gmail.com>
2022-05-20 02:13:31 +0530

    manager 04 200522

--- include/config.php
+++ include/config.php
@@ -7,5 +7,4 @@ define( 'SSOAPI', 'http://administrator.cafecenter.in/wp-json/api/' );
 ini_set('ignore_repeated_errors', TRUE); 
 ini_set('display_errors', FALSE);
 ini_set('log_errors', TRUE); 
-
 ini_set('error_log', 'errors.log'); 
