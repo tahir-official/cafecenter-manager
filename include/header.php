3113f35f446903aca4c276e742cb37eab0992302
tahir-official <tahirk.official@gmail.com>
2022-09-04 05:36:05 +0530

    02 040922

--- include/header.php
+++ include/header.php
@@ -10,7 +10,7 @@
    if(ENV=='prod'){
     $site_url=$portal_detail->MANAGER_PORTAL_URL;
    }else{
-    $site_url='https://localhost/php/cafecenter-manager/';
+    $site_url=LOCAL_URL;
    }
    if(!isset($_SESSION['is_manager_logged_in'])){ $commonFunction->redirect('index.php'); }
 ?>
@@ -84,7 +84,7 @@
     <?php
    }
    
-   
+
    ?>
   </script>
   <style>
